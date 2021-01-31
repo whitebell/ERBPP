b06d6c6c6a90dc4f1ddd0c7b31f3861030a7509c
whitebell <nikky.hh@gmail.com>
2021-02-01 07:23:41 +0900

    super awesome commit

---
+++ ERBPP/Program.cs
@@ -0,0 +1,466 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Whitebell.Library.Utils;
+using System.Collections.Generic;
+
+namespace ERBPP
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+#if DEBUG
+            using var fs = File.Open("D:\\Game\\eratoho\\EVENT_K14\\EVENT_K14.ERB", FileMode.Open, FileAccess.Read);
+            using var sr = new StreamReader(fs, Encoding.UTF8);
+            using var fw = File.Open("D:\\Game\\eratoho\\EVENT_K14\\EVENT_K14.PP.ERB", FileMode.Create, FileAccess.Write);
+            using var sw = new StreamWriter(fw, Encoding.UTF8);
+#elif RELEASE
+            var sr = new StreamReader(Console.OpenStandardInput(), Encoding.UTF8);
+            var sw = new StreamWriter(Console.OpenStandardOutput(), Encoding.UTF8);
+#endif
+
+            string l;
+            var curIndentLv = 0;
+            var prevType = LineType.Unknown;
+            while ((l = sr.ReadLine()?.TrimStart()) is not null)
+            {
+            REDO:
+
+                var t = new PseudoLexer(l).GetToken();
+
+                switch (t.Type)
+                {
+                    case LineType.Blank:
+                        sw.WriteLine();
+                        break;
+
+                    case LineType.FunctionDefinition:
+                        if (curIndentLv != 0)
+                            throw new Exception();
+
+                        sw.WriteLine(l);
+                        break;
+
+                    case LineType.StartRegionComment:
+                    case LineType.EndRegionComment:
+                        sw.WriteLine(l);
+                        break;
+
+                    case LineType.If:
+                    case LineType.Repeat:
+                    case LineType.While:
+                    case LineType.For:
+                    case LineType.SelectCase:
+                    case LineType.PrintData:
+                        sw.Write(new string('\t', curIndentLv++));
+                        sw.WriteLine(l);
+                        break;
+                    case LineType.ElseIf:
+                    case LineType.Else:
+                        sw.Write(new string('\t', curIndentLv - 1));
+                        sw.WriteLine(l);
+                        break;
+                    case LineType.Endif:
+                    case LineType.Rend:
+                    case LineType.Wend:
+                    case LineType.Next:
+                    case LineType.EndData:
+                        sw.Write(new string('\t', --curIndentLv));
+                        sw.WriteLine(l);
+                        break;
+                    case LineType.Case:
+                        if (prevType == LineType.SelectCase)
+                            sw.Write(new string('\t', curIndentLv++));
+                        else
+                            sw.Write(new string('\t', curIndentLv - 1));
+                        sw.WriteLine(l);
+                        break;
+                    case LineType.CaseElse:
+                        sw.Write(new string('\t', curIndentLv - 1));
+                        sw.WriteLine(l);
+                        break;
+                    case LineType.EndSelect:
+                        curIndentLv -= 2;
+                        sw.Write(new string('\t', curIndentLv));
+                        sw.WriteLine(l);
+                        break;
+
+                    case LineType.Sif:
+                        sw.Write(new string('\t', curIndentLv));
+                        sw.WriteLine(l);
+                        if ((l = sr.ReadLine()?.TrimStart()) is null)
+                            throw new Exception();
+                        sw.Write(new string('\t', curIndentLv + 1));
+                        sw.WriteLine(l);
+                        break;
+
+                    case LineType.Comment:
+                        var lst = new List<(LineType, string)> { (t.Type, l) };
+                        string nl;
+                        LineType nextType = LineType.Unknown;
+                        while ((nl = sr.ReadLine()?.TrimStart()) is not null)
+                        {
+                            var nt = new PseudoLexer(nl).GetToken();
+                            switch (nt.Type)
+                            {
+                                case LineType.Blank:
+                                case LineType.Comment:
+                                case LineType.StartRegionComment:
+                                case LineType.EndRegionComment:
+                                    lst.Add((nt.Type, nl));
+                                    break;
+                                default:
+                                    nextType = nt.Type;
+                                    goto BREAK;
+                            }
+                        }
+                    BREAK:
+                        switch (nextType)
+                        {
+                            case LineType.Call:
+                            case LineType.Return:
+                            case LineType.BuiltinFunction:
+                            case LineType.FunctionDefinition:
+                            case LineType.Variable:
+                            case LineType.Endif:
+                            case LineType.If:
+                            case LineType.Sif:
+                            case LineType.SelectCase:
+                                foreach (var e in lst)
+                                {
+                                    sw.Write(new string('\t', curIndentLv));
+                                    sw.WriteLine(e.Item2);
+                                }
+                                break;
+                            case LineType.ElseIf:
+                            case LineType.Else:
+                            case LineType.Case:
+                            case LineType.CaseElse:
+                            case LineType.Repeat:
+                                foreach (var e in lst)
+                                {
+                                    if (e.Item1 != LineType.Blank)
+                                    {
+                                        var indentLv = nextType == LineType.Case && prevType == LineType.SelectCase ? curIndentLv : Math.Max(0, curIndentLv - 1);
+                                        sw.Write(new string('\t', indentLv));
+                                        sw.WriteLine(e.Item2);
+                                    }
+                                    else
+                                    {
+                                        sw.WriteLine();
+                                    }
+                                }
+                                    break;
+                            default:
+                                throw new Exception();
+                        }
+                        l = nl;
+                        goto REDO;
+
+                    default:
+                        sw.Write(new string('\t', curIndentLv));
+                        sw.WriteLine(l);
+                        break;
+                }
+
+                if (t.Type != LineType.Blank && t.Type != LineType.Comment && t.Type != LineType.StartRegionComment && t.Type != LineType.EndRegionComment)
+                    prevType = t.Type;
+            }
+        }
+    }
+
+    public class PseudoLexer
+    {
+        private readonly StringStream ss;
+
+        private static readonly HashSet<string> variable = new();
+
+        public PseudoLexer(string s) => ss = new StringStream(s.TrimEnd('\r', '\n'));
+
+        private void SkipSpace()
+        {
+            while (!ss.EndOfStream && Char.IsWhiteSpace(ss.Current))
+                ss.NextChar(out _);
+        }
+
+        //private void Skip(int i)
+        //{
+        //    var s = 0;
+        //    while (!ss.EndOfStream && s++ < i)
+        //        ss.NextChar(out _);
+        //}
+
+        private void Consume(char c)
+        {
+            if (!ss.EndOfStream && ss.Current == c)
+                ss.NextChar(out _);
+        }
+
+        private string GetIdent()
+        {
+            var sb = new StringBuilder();
+            while (!ss.EndOfStream && Char.IsLetterOrDigit(ss.Current) && !IsVariableSeparator(ss.Current))
+            {
+                ss.NextChar(out var c);
+                sb.Append(c);
+            }
+            return sb.ToString();
+        }
+
+        public Token GetToken()
+        {
+            SkipSpace();
+
+            if (ss.EndOfStream)
+            {
+                return new Token { Type = LineType.Blank };
+            }
+            else if (IsCommentStart(ss.Current))
+            {
+                Consume(';');
+                SkipSpace();
+                if (ss.Current == '#')
+                {
+                    Consume('#');
+                    SkipSpace();
+                }
+
+                var ident = GetIdent().ToLower();
+
+                switch (ident)
+                {
+                    case "region":
+                        return new Token { Type = LineType.StartRegionComment };
+                    case "endregion":
+                        return new Token { Type = LineType.EndRegionComment };
+                    default:
+                        return new Token { Type = LineType.Comment };
+                }
+            }
+            else if (IsFunctionStart(ss.Current))
+            {
+                return new Token { Type = LineType.FunctionDefinition };
+            }
+            else if (IsAttributeStart(ss.Current))
+            {
+                Consume('#');
+                var ident = GetIdent().ToUpper();
+                switch (ident)
+                {
+                    case "FUNCTION":
+                    case "LATER":
+                    case "PRI":
+                        return new Token { Type = LineType.Attribute };
+                    case "DIM":
+                    case "DIMS":
+                        {
+                            SkipSpace();
+                            var v = GetIdent();
+                            if (!variable.Contains(v))
+                                variable.Add(v);
+                            return new Token { Type = LineType.VariableDefinition };
+                        }
+                    default:
+                        return new Token { Type = LineType.Unknown };
+                }
+            }
+            else
+            {
+                var ident = GetIdent().ToUpper();
+                switch (ident)
+                {
+                    case "SIF":
+                        return new Token { Type = LineType.Sif };
+
+                    case "IF":
+                        return new Token { Type = LineType.If };
+                    case "ELSEIF":
+                        return new Token { Type = LineType.ElseIf };
+                    case "ELSE":
+                        return new Token { Type = LineType.Else };
+                    case "ENDIF":
+                        return new Token { Type = LineType.Endif };
+
+                    case "REPEAT":
+                        return new Token { Type = LineType.Repeat };
+                    case "REND":
+                        return new Token { Type = LineType.Rend };
+
+                    case "SELECTCASE":
+                        return new Token { Type = LineType.SelectCase };
+                    case "CASE":
+                        return new Token { Type = LineType.Case };
+                    case "CASEELSE":
+                        return new Token { Type = LineType.CaseElse };
+                    case "ENDSELECT":
+                        return new Token { Type = LineType.EndSelect };
+
+                    case "FOR":
+                        return new Token { Type = LineType.For };
+                    case "NEXT":
+                        return new Token { Type = LineType.Next };
+
+                    case "WHILE":
+                        return new Token { Type = LineType.While };
+                    case "WEND":
+                        return new Token { Type = LineType.Wend };
+
+                    case "BREAK":
+                        return new Token { Type = LineType.Break };
+
+                    case "CALL":
+                    case "CALLFORM":
+                    case "TRYCALL":
+                    case "TRYCALLFORM":
+                        return new Token { Type = LineType.Call };
+
+                    case "RETURN":
+                    case "RETURNF":
+                        return new Token { Type = LineType.Return };
+
+                    case "THROW":
+                        return new Token { Type = LineType.Throw };
+
+                    case "PRINTDATA":
+                    case "PRINTDATAW":
+                        return new Token { Type = LineType.PrintData };
+                    case "DATA":
+                    case "DATAFORM":
+                        return new Token { Type = LineType.Data };
+                    case "ENDDATA":
+                        return new Token { Type = LineType.EndData };
+
+                    case "DRAWLINE":
+                    case "EXISTCSV":
+                    case "FONTBOLD":
+                    case "FONTREGULAR":
+                    case "PRINT":
+                    case "PRINTL":
+                    case "PRINTW":
+                    case "PRINTFORM":
+                    case "PRINTFORMDW":
+                    case "PRINTFORML":
+                    case "PRINTFORMW":
+                    case "QUIT":
+                    case "SETBIT":
+                        return new Token { Type = LineType.BuiltinFunction };
+
+                    case "ARG":
+                    case "BASE":
+                    case "CFLAG":
+                    case "EXP":
+                    case "FLAG":
+                    case "LOCAL":
+                    case "RESULT":
+                    case "RESULTS":
+                    case "SELECTCOM":
+                    case "STAIN":
+                    case "TALENT":
+                    case "TCVAR":
+                    case "TFLAG":
+                    case "S":
+                        return new Token { Type = LineType.Variable };
+                    default:
+                        if (variable.Contains(ident))
+                            return new Token { Type = LineType.Variable };
+                        throw new Exception();
+                }
+            }
+        }
+
+        private static bool IsCommentStart(char c) => c == ';';
+        private static bool IsFunctionStart(char c) => c == '@';
+        private static bool IsAttributeStart(char c) => c == '#';
+        private static bool IsVariableSeparator(char c) => c == ':';
+        //private static bool IsIdentStart(char c) => Char.IsLetter(c);
+
+        //private Token Ident() => throw new NotImplementedException();
+    }
+
+    public class Token
+    {
+        public string Value { get; init; } = "";
+        public LineType Type { get; init; } = LineType.Unknown;
+    }
+
+    public enum LineType
+    {
+        /// <summary>
+        /// 空行
+        /// </summary>
+        Blank,
+
+        /// <summary>
+        /// コメント行
+        /// </summary>
+        Comment,
+
+        /// <summary>
+        /// リージョンコメント開始
+        /// </summary>
+        StartRegionComment,
+        /// <summary>
+        /// リージョンコメント終了
+        /// </summary>
+        EndRegionComment,
+
+        /// <summary>
+        /// 関数定義
+        /// </summary>
+        FunctionDefinition,
+
+        /// <summary>
+        /// 関数属性 PRI, LATER, etc...
+        /// </summary>
+        Attribute,
+
+        VariableDefinition,
+
+        Sif,
+
+        If,
+        Else,
+        ElseIf,
+        Endif,
+
+        Repeat,
+        Rend,
+
+        SelectCase,
+        Case,
+        CaseElse,
+        EndSelect,
+
+        For,
+        Next,
+
+        While,
+        Wend,
+
+        Break,
+
+        Call,
+
+        Return,
+
+        Throw,
+
+        PrintData,
+        Data,
+        EndData,
+
+        /// <summary>
+        /// 組み込み関数
+        /// </summary>
+        BuiltinFunction,
+
+        /// <summary>
+        /// 変数
+        /// </summary>
+        Variable,
+
+        Unknown,
+    }
+}
---
+++ ERBPP/StringStream.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Whitebell.Library.Utils
+{
+    /// <summary>文字列を1字ずつ評価するクラス</summary>
+    [Serializable]
+    public class StringStream : Stream
+    {
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private readonly string _str;
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private long _ptr;
+
+        #region Constructors
+
+        public StringStream()
+        {
+            _str = "";
+            _ptr = 0;
+        }
+
+        public StringStream(string str)
+        {
+            _str = str ?? "";
+            _ptr = 0;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public override bool CanRead => true;
+
+        public override bool CanSeek => true;
+
+        public override bool CanWrite => false;
+
+        /// <summary>末尾かどうかをしめす値を取得します。</summary>
+        public bool EndOfStream => _ptr >= _str.Length;
+
+        /// <summary>内部文字列の文字数を取得します。</summary>
+        public override long Length => _str.Length;
+
+        /// <summary>内部ポインタ位置を取得または設定します。</summary>
+        /// <exception cref="ArgumentOutOfRangeException">負の位置、または文字列長を越えた位置に内部ポインタを設定しようとした場合</exception>
+        public override long Position
+        {
+            get => _ptr;
+            set
+            {
+                if (value < 0 || _str.Length < value)
+                    throw new ArgumentOutOfRangeException(nameof(value));
+                _ptr = value;
+            }
+        }
+
+        /// <summary>内部文字列を取得します。</summary>
+        public string RawString => _str;
+
+        #endregion
+
+        /// <summary>指定した <see cref="Object"/> が現在の <see cref="StringStream"/>  と等しいかどうかを判断します。</summary>
+        /// <param name="obj">現在の <see cref="StringStream"/> と比較する <see cref="Object"/>。</param>
+        /// <returns>指定した <see cref="Object"/> が現在の <see cref="StringStream"/> と等しい場合は true。それ以外の場合は false。</returns>
+        public override bool Equals(object obj) => obj is StringStream s && Equals(s);
+
+        /// <summary>指定した <see cref="StringStream"/> が現在の <see cref="StringStream"/> と等しいかどうかを判断します。</summary>
+        /// <param name="ss">現在の <see cref="StringStream"/> と比較する <see cref="StringStream"/>。</param>
+        /// <returns>指定した <see cref="StringStream"/> が現在の <see cref="StringStream"/> と等しい場合は true。それ以外の場合は false。</returns>
+        public bool Equals(StringStream ss) => ss is not null && _str == ss._str && _ptr == ss._ptr;
+
+        public override void Flush()
+        { /* no-op */ }
+
+        /// <summary>この <see cref="StringStream"/> のハッシュコードを返します。</summary>
+        /// <returns>32 ビット符号付整数ハッシュコード</returns>
+        public override int GetHashCode() => _str.GetHashCode();
+
+        /// <param name="buffer"></param>
+        /// <param name="offset"></param>
+        /// <param name="count"></param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        /// <exception cref="ArgumentNullException"></exception>
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            if (_str.Length < offset + count)
+                throw new ArgumentOutOfRangeException($"{nameof(offset)} and/or {nameof(count)}" ,"The sum of offset and count is larger than the buffer length.");
+
+            if (buffer == null)
+                throw new ArgumentNullException(nameof(buffer));
+
+            if (offset < 0)
+                throw new ArgumentOutOfRangeException(nameof(offset));
+            if (count < 0)
+                throw new ArgumentOutOfRangeException(nameof(count));
+
+            var len = Math.Min((int)_ptr + count, _str.Length);
+            var s = _str.Substring((int)_ptr, len);
+
+            for (var i = 0; i < s.Length; i++)
+                buffer[offset + i] = (byte)s[i];
+
+            return len;
+        }
+
+        /// <summary>末尾でなければ現在の文字をout引数にセットし、ポインタを1進める。末尾ならばnull文字をセットする。</summary>
+        /// <param name="chr"></param>
+        public virtual bool NextChar(out char chr)
+        {
+            _ptr++;
+            if (_ptr <= _str.Length)
+            {
+                chr = _str[(int)_ptr - 1];
+                return true;
+            }
+            else
+            {
+                chr = '\0';
+                return false;
+            }
+        }
+
+        /// <summary>カウンタを進めずに現在の文字を返す。末尾の場合はnull文字を返す。</summary>
+        public char Current => _ptr < _str.Length ? _str[(int)_ptr] : '\0';
+
+        /// <summary>カウンタを進めずに現在の文字を返す。末尾の場合はnull文字を返す。</summary>
+        public char Peek() => Current;
+
+        /// <summary>カウンタを進めずに指定した数読み呼ばした先の文字を返す。末尾の場合はnull文字を返す。</summary>
+        /// <param name="skipLength"></param>
+        public char Peek(int skipLength) => _ptr + skipLength < _str.Length ? _str[(int)_ptr + skipLength] : '\0';
+
+        /// <summary>ポインタ位置を 0 に戻します。</summary>
+        public void Reset() => _ptr = 0;
+
+        /// <param name="offset"></param>
+        /// <param name="origin"></param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            var tmp_ptr = _ptr;
+            switch (origin)
+            {
+                case SeekOrigin.Begin:
+                    tmp_ptr = offset;
+                    break;
+                case SeekOrigin.Current:
+                    tmp_ptr += offset;
+                    break;
+                case SeekOrigin.End:
+                    tmp_ptr = _str.Length + offset;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(origin));
+            }
+            if (tmp_ptr < 0 || tmp_ptr > _str.Length)
+                throw new ArgumentOutOfRangeException(nameof(offset));
+            _ptr = tmp_ptr;
+            return _ptr;
+        }
+
+        /// <param name="value"></param>
+        /// <exception cref="NotSupportedException"></exception>
+        public override void SetLength(long value) => throw new NotSupportedException();
+
+        /// <summary>文字列表現。内部文字列を返す。</summary>
+        public override string ToString() => _str;
+
+        /// <param name="buffer"></param>
+        /// <param name="offset"></param>
+        /// <param name="count"></param>
+        /// <exception cref="NotSupportedException"></exception>
+        public override void Write(byte[] buffer, int offset, int count) => throw new NotSupportedException();
+
+        public static implicit operator StringStream(string s) => new StringStream(s ?? "");
+
+        public static explicit operator string(StringStream s) => s is not null ? s.RawString : throw new ArgumentNullException(nameof(s));
+    }
+}
