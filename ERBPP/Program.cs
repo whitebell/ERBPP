4d05ca76318564a109d5e905583a3e471763240d
whitebell <nikky.hh@gmail.com>
2021-02-01 11:00:47 +0900

    変数追加

--- ERBPP/Program.cs
+++ ERBPP/Program.cs
@@ -353,6 +353,7 @@ namespace ERBPP
                     case "EXP":
                     case "FLAG":
                     case "LOCAL":
+                    case "LOCALS":
                     case "RESULT":
                     case "RESULTS":
                     case "SELECTCOM":
@@ -360,7 +361,32 @@ namespace ERBPP
                     case "TALENT":
                     case "TCVAR":
                     case "TFLAG":
+                    case "A":
+                    case "B":
+                    case "C":
+                    case "D":
+                    case "E":
+                    case "F":
+                    case "G":
+                    case "H":
+                    case "I":
+                    case "J":
+                    case "K":
+                    case "L":
+                    case "M":
+                    case "N":
+                    case "O":
+                    case "P":
+                    case "Q":
+                    case "R":
                     case "S":
+                    case "T":
+                    case "U":
+                    case "V":
+                    case "W":
+                    case "X":
+                    case "Y":
+                    case "Z":
                         return new Token { Type = LineType.Variable };
                     default:
                         if (variable.Contains(ident))
